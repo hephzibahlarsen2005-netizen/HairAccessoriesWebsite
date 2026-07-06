65852df0980dc0db1d3c4af07cd7b0d8d890c9c8
hephzibahlarsen2005-netizen <hephzibahlarsen2005@gmail.com>
2026-07-06 17:34:51 +0530

    Added Render configuration

--- app.py
+++ app.py
@@ -18,5 +18,7 @@ def about():
 def contact():
     return send_from_directory(".", "contact.html")
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
